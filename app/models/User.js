44f31abc2d77a931b7818bf6dde8c8fab9925a0c
OrlSan <orlsan@ciencias.unam.mx>
2016-08-18 16:15:16 -0500

    Add User passwords with Bcrypt

--- app/models/User.js
+++ app/models/User.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var UserSchema = mongoose.Schema({
   name: String,
@@ -10,6 +11,21 @@ var UserSchema = mongoose.Schema({
   password: String,
   age: Number
 });
+
+UserSchema.methods.generateHash = function(password, callback) {
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      console.log("Error al crear el password: " + err);
+      return callback(err);
+    }
+
+    bcrypt.hash(password, salt, callback);
+  });
+};
+
+UserSchema.methods.checkPassword = function(inputPass, callback) {
+  bcrypt.compare(myPlaintextPassword, hash, callback);
+};
 
 // Exportamos el modelo
 module.exports = mongoose.model('User', UserSchema);
